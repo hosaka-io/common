481f4719bdda98f25b0e1c2bb114a5093ca18feb
Brian Edwards <brian@edwardstx.us>
2018-05-28 14:05:28 -0700

    Added common user

--- project.clj
+++ project.clj
@@ -1,12 +1,16 @@
-(defproject io.hosaka/common "1.0.0"
+(defproject io.hosaka/common "1.1.0"
   :description "Hosaka common library"
   :url "https://github.com/hosaka-io/common"
-  :repositories ^:replace [["releases" "https://artifactory.i.hosaka.io/artifactory/libs-release"]
-                           ["snapshots" "https://artifactory.i.hosaka.io/artifactory/libs-snapshot"]]
+  :repositories ^:replace [["releases" {:url  "https://artifactory.i.hosaka.io/artifactory/libs-release"
+                                        :username :env/lein_username
+                                        :password :env/lein_password
+                                        :sign-releases false}]
+                                  ["snapshots" "https://artifactory.i.hosaka.io/artifactory/libs-snapshot"]]
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/tools.logging "0.4.0"]
+                 [org.clojure/core.cache "0.7.1"]
                  [cheshire "5.8.0"]
                  [com.stuartsierra/component "0.3.2"]
                  [yada "1.2.11"]
@@ -14,7 +18,7 @@
                  [manifold "0.1.7-alpha6"]
                  [clj-time "0.14.4"]
                  [com.layerware/hugsql "0.4.8"]
-                 [hikari-cp "2.3.0"]
+                 [hikari-cp "2.4.0"]
                  [org.flywaydb/flyway-core "5.0.7"]]
   :target-path "target/%s"
   )
